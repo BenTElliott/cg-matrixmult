fe6d0257f13d8a77c120805e4ec1cbf6b0d45548
Ben Elliott <ben.elliott243@gmail.com>
2020-03-11 12:40:33 -0500

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -4,27 +4,21 @@ var compound_transform;
 function CalculateCompoundTransform(transforms) {
     // matrices in `transforms[i].mat4x4`
     // note `transform[0]` is first tranform to apply to vertex
-    
-    // if only one transform, set compound transform equal to it
+   
+    // if only one transform, set compound transform eequal to it
     // otherwise multiply all matrices together (in proper order)
-    // `compound_transform = Matrix.multiply(...)`
-    var tranform_matrices = [];
-	
-    compound_transform = new Matrix(4, 4); // change / remove this
-	if(transforms.length == 1)
+	// `compound_transform = Matrix.multiply(...)`
+	var transform_matrices = [];
+	 
+	if (transforms.length === 1) 
 	{
-		compound_transform = transforms[0];
-	}
-	else if(transforms.length > 1)
+        compound_transform = transforms[0].mat4x4;
+    }
+    else 
 	{
-		compound_transform = transforms[0].mat4x4;
-		for(var i = 1; i < transforms.length; i++)
-		{
-			tranform_matrices.push(transforms[i].mat4x4);
-		}
-		compound_transform = Matrix.multiply(tranform_matrices);
-		console.log(compound_transform);
-	}
+        transforms.map((transform) => { transform_matrices.push(transform.mat4x4); })
+        compound_transform = Matrix.multiply(transform_matrices);
+    }
 
     return compound_transform;
 }
@@ -32,23 +26,38 @@ function CalculateCompoundTransform(transforms) {
 // automatically called whenever compound transform changes
 function CalculateTransformedVertex(vertex) {
     // multiple vertex by compound_transform
-    // `final_vertex = Matrix.multiply(...)`
-	var hold = [];
-	hold[0] = CalculateCompoundTransform(app.transforms);
-	hold[1] = vertex;
-    var final_vertex = Matrix.multiply(hold); 
-	
+	// `final_vertex = Matrix.multiply(...)`
+    var final_vertex = Matrix.multiply([compound_transform, vertex]);
     return final_vertex;
 }
 
 // automatically called whenever user modifies a transform (changes type or values)
 function ChangeTransform(index, type, values) {
+	
     app.transforms[index].type = type;
-    // update `app.transforms[index].mat4x4`
-	if(type.toLowerCase() == "translate") 
+	// update `app.transforms[index].mat4x4`
+	var conversion = Math.PI / 180;
+    if (type === "translate")     //translate
 	{
-		Mat4x4Translate(app.transforms[index].mat4x4, values[0], values[1], values[2]);
-	}
+        Mat4x4Translate(app.transforms[index].mat4x4, values[0], values[1], values[2]);
+    } 
+	else if (type === "scale") 	  //scale
+	{
+        Mat4x4Scale(app.transforms[index].mat4x4, values[0], values[1], values[2]);
+    } 
+	else if (type === "rotate_x") //rotate_x
+	{
+        Mat4x4RotateX(app.transforms[index].mat4x4, values[0] * conversion);
+    } 
+	else if (type === "rotate_y") //rotate_y
+	{
+        Mat4x4RotateY(app.transforms[index].mat4x4, values[0] * conversion);
+    } 
+	else if (type === "rotate_z") //rotate_z
+	{
+        Mat4x4RotateZ(app.transforms[index].mat4x4, values[0] * conversion);
+    }
+
     // recalculate compound transform and tranformed vertex
     app.compound = CalculateCompoundTransform(app.transforms);
     app.final_vertex = CalculateTransformedVertex(app.vertex);
